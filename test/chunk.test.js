3c94fff9143b0dbf38cf900d98d19bd0b268f17e
Joni Flink <joni.flink@tuni.fi>
2025-12-04 10:18:46 +0200

    fix tests

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -7,22 +7,22 @@ describe("chunk.js", () => {
     it("should retunr two 2 item arrays with array size 2", () => {
         expect(chunk(array, 2)).to.deep.equal([['a', 'b'], ['c', 'd']])
     })
-    it("should retunr empty arrays with array size -3", () => {
-        expect(chunk(array, -3)).to.equal([])
+    it("should return empty arrays with array size -3", () => {
+        expect(chunk(array, -3)).to.deep.equal([])
     })
-    it("should retunr array arrays with no array size", () => {
+    it("should return array arrays with no array size", () => {
         expect(chunk(array, )).to.deep.equal(array)
     })
-    it("should retunr empty arrays with no array", () => {
-        expect(chunk([],1)).to.equal([])
+    it("should return empty arrays with no array", () => {
+        expect(chunk([],1)).to.deep.equal([])
     })
     it("should throw exception with input as 'string' ", () => {
         expect(()=>chunk('string', 1)).to.throw()
     })
-    it("should retunr empty with input as null ", () => {
+    it("should return empty with input as null ", () => {
         expect(() => chunk(null, 2)).to.deep.equal([])
     })
-    it("should retunr empty with undefined array ", () => {
+    it("should return empty with undefined array ", () => {
         expect(chunk(undefined, 2)).to.deep.equal([])
     })
 })
