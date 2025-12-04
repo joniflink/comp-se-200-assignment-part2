64c42069a59191dda8fb1310d7e21038799cf33c
Joni Flink <joni.flink@tuni.fi>
2025-12-04 10:29:54 +0200

    add more tests for slice

--- test/slice.test.js
+++ test/slice.test.js
@@ -21,4 +21,20 @@ describe("slice.js", () => {
     it("should return [2, 3] with array as [1, 2, 3, 4], start as -3 and end as -1", () => {
         expect(slice([1, 2, 3, 4], -3, -1)).to.deep.equal([2, 3])
     })
+
+    it("should return [] (empty array) with array as null", () => {
+        expect(slice(null)).to.deep.equal([])
+    })
+
+    it("should return [1, 2, 3, 4] with array as [1, 2, 3, 4], start as null", () => {
+        expect(slice([1, 2, 3, 4], null)).to.deep.equal([1, 2, 3, 4])
+    })
+
+    it("should return [1, 2, 3, 4] with array as [1, 2, 3, 4] and start as -10", () => {
+        expect(slice([1, 2, 3, 4], -10)).to.deep.equal([1, 2, 3, 4])
+    })
+
+    it("should return [] (empty array) with array as [1, 2, 3, 4], start as 3 and end as 2", () => {
+        expect(slice([1, 2, 3, 4], 3, 2)).to.deep.equal([])
+    })
 })
